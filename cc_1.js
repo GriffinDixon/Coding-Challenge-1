34ab646e46073d3650952ec0d95260ae4345291f
Griffin Dixon <gtd0604@icloud.com>
2024-09-01 12:49:59 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let customerName = "String Cheese";
+const customerID = 556;
+var isPremiumMember = true;
+
+console.log(customerName, typeof(customerName))
+console.log(customerID, typeof(customerID))
+console.log(isPremiumMember, typeof(isPremiumMember))
