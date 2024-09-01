35fe01e3611398dc4db0e9b086e37de0f6e5b69d
Griffin Dixon <gtd0604@icloud.com>
2024-09-01 13:17:01 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -1,9 +1,21 @@
 // Task 1: Variables and Data Types
 
-let customerName = "String Cheese";
-const customerID = 556;
-var isPremiumMember = true;
+let employeeName = "String Cheese";
+const employeeID = 556;
+var isActive = true;
 
-console.log(customerName, typeof(customerName))
-console.log(customerID, typeof(customerID))
-console.log(isPremiumMember, typeof(isPremiumMember))
+console.log(employeeName, typeof(employeeName))
+console.log(employeeID, typeof(employeeID))
+console.log(isActive, typeof(isActive))
+
+
+
+// Task 2: Compound Data Types
+
+let products = ["Macbook Pro", "Keyboard", "Mouse"];
+const productDetails = {
+    name: technology,
+    price: 240.00,
+    inStock: true
+}
+console. log(products, productDetails)
